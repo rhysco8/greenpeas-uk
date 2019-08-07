f932549c611bd8d3f2b817cb4f0688edb1d05ec6
Celia <celiafrom12@gmail.com>
2019-08-07 17:23:23 +0100

    feat: scanner controller, not finished

--- guilt-tripper/ScannerViewController.swift
+++ guilt-tripper/ScannerViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import AVFoundation
+import UIKit
 
 class Scanner: NSObject
 {
@@ -16,47 +17,175 @@ class Scanner: NSObject
     
     private func createCaptureSession() ->AVCaptureSession?
     {
+        let captureSession = AVCaptureSession()
         
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+          return nil
+        }
+    
+        do
+        {
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            let metaDataOutput = AVCaptureMetadataOutput()
+            
+            if captureSession.canAddInput(deviceInput)
+            {
+                captureSession.addInput(deviceInput)
+            }
+            else
+            {
+                return nil
+            }
+            
+            if captureSession.canAddOutput(metaDataOutput)
+            {
+                 captureSession.addOutput(metaDataOutput)
+            
+                if let viewController = self.viewController as? AVCaptureMetadataOutputObjectsDelegate
+                {
+                    metaDataOutput.setMetadataObjectsDelegate(viewController,
+                                                                queue: DispatchQueue.main)
+                    metaDataOutput.metadataObjectTypes = self.metaObjectTypes()
+                }
+            }
+            else
+            {
+              return nil
+            }
+        }
+        catch
+        {
+             return nil
+        }
+        return captureSession
     }
     
-    let captureSession = AVCaptureSession()
-    guard let captureDevice = AVCaptureDevice.default(for: .video) else {
-    return nil
+   
+    
+    private func metaObjectTypes() -> [AVMetadataObject.ObjectType]
+    {
+        return [.qr,
+        .code128,
+        .code39,
+        .code39Mod43,
+        .code93,
+        .ean13,
+        .ean8,
+        .interleaved2of5,
+        .itf14,
+        .pdf417,
+        .upce
+        ]
     }
     
-    do
+    private func createPreviewLayer(withCaptureSession captureSession: AVCaptureSession,
+                                    view: UIView) -> AVCaptureVideoPreviewLayer
     {
-    if captureSession.canAddInput(deviceInput)
-    {
-    captureSession.addInput(deviceInput)
-    }
-    else
-    {
-    return nil
-    }
-    
-    if captureSession.canAddOutput(metaDataOutput)
-    {
-    captureSession.addOutput(metaDataOutput)
-    if let viewController = self.viewController as? AVCaptureMetadataOutputObjectsDelegate
-    {
-    metaDataOutput.setMetadataObjectsDelegate(viewController,
-                                                queue: DispatchQueue.main)
-    metaDataOutput.metadataObjectTypes = self.metaObjectTypes()
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        
+        return previewLayer
     }
     
-    }
-    else
-    {
-    return nil
-    }
-    }
-    catch
-    {
-    return nil
-    }
-    return captureSession
     
-    let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
-    let metaDataOutput = AVCaptureMetadataOutput()
+    
+    init(withViewController viewController: UIViewController,
+              view: UIView,
+              codeOutputHandler: @escaping (String) -> Void)
+    {
+        self.viewController = viewController
+        self.codeOutputHandler = codeOutputHandler
+        
+        super.init()
+        
+        if let captureSession = self.createCaptureSession()
+        {
+            self.captureSession = captureSession
+            let previewLayer = self.createPreviewLayer(withCaptureSession: captureSession, view: view)
+            view.layer.addSublayer(previewLayer)
+        }
+    }
+    
+    func requestCaptureSessionStartRunning()
+    {
+        guard let captureSession = self.captureSession else {
+            return
+        }
+        
+        if !captureSession.isRunning
+        {
+            captureSession.startRunning()
+        }
+    }
+    
+    
+    func requestCaptureSessionStopRunning()
+    {
+        guard let captureSession = self.captureSession else {
+            return
+    }
+        if captureSession.isRunning
+        {
+            captureSession.stopRunning()
+        }
+    }
+   
+    func scannerDelegate(_ output: AVCaptureMetadataOutput,
+                         didOutput metadataObjects: [AVMetadataObject],
+                         from connection: AVCaptureConnection)
+    {
+        self.requestCaptureSessionStopRunning()
+        
+        if let metaDataObject = metadataObjects.first
+        {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else
+            {
+                return
+            }
+            
+            guard let stringValue = readableObject.stringValue else
+            {
+                return
+            }
+            
+            self.codeOutputHandler(stringValue)
+        }
+    }
 }
+
+
+//var scanner: Scanner?
+//
+//
+//
+//self.scanner = Scanner(withViewController: self,
+//                       view:self.view,
+//                       codeOutputHandler: self.handleCode)
+//if let scanner = self.scanner
+//{
+//    scanner.requestCaptureSessionStartRunning()
+//}
+//
+//
+//
+//func handleCode(code: String)
+//{
+//    print(code)
+//}
+//
+//
+//
+//
+//}
+//
+//
+//public func metadataOutput(_ output: AVCaptureMetadataOutput,
+//                           didOutput metadataObjects: [AVMetadataObject],
+//                           from connection: AVCaptureConnection) {
+//    self.scanner?.scannerDelegate(output,
+//                                  didOutput: metadataObjects,
+//                                  from: connection)
+//}
+
+//import AVFoundation AVCapturemetadata add do viewcontroller?
