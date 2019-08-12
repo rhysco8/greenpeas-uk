ecda1cced9c7558c10a2e75c8fa8d5cae1572bcb
Papya Qureshi <papyaqureshi@gmail.com>
2019-08-12 15:22:52 +0100

    feat: connect next product button to view controller

---
+++ guilt-tripper/WellDoneViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WellDoneViewController.swift
+//  guilt-tripper
+//
+//  Created by Student on 12/08/2019.
+//  Copyright © 2019 Greenpeas UK. All rights reserved.
+//
+
+import UIKit
+
+class WellDoneViewController: UIViewController {
+
+    @IBOutlet weak var nextProductButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
