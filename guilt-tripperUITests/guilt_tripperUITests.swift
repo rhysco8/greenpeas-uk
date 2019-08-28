b8bfe050260e1a0d709e77bc703e4720ab9d2578
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-08-28 17:59:44 +0100

    test: add feature tests for no palm oil product and product not found
    Co-authored-by: Rhys Collier <rhys.collier@hotmail.co.uk>

--- guilt-tripperUITests/guilt_tripperUITests.swift
+++ guilt-tripperUITests/guilt_tripperUITests.swift
@@ -30,7 +30,6 @@ class guilt_tripperUITests: XCTestCase {
     }
 
     func testProductWithPalmOil() {
-        
         let app = XCUIApplication()
         app.buttons["ENTER"].tap()
         
@@ -38,11 +37,42 @@ class guilt_tripperUITests: XCTestCase {
         let product = "Nutella"
         productTextField.tap()
         productTextField.typeText(product)
-        
         app.buttons["SUBMIT"].tap()
+        
+        XCTAssertTrue(app.images["sad orangutan 1"].exists)
+        XCTAssertTrue(app.images["sad orangutan 2"].exists)
         app.buttons["FIND OUT MORE"].tap()
+        
         app.children(matching: .window).element(boundBy: 0).children(matching: .other).element(boundBy: 2).children(matching: .other).element.children(matching: .other).element(boundBy: 0).buttons["READ MORE ..."].tap()
         app.buttons["Done"].tap()
+        app.buttons["ENTER ANOTHER PRODUCT"].tap()
+    }
+    
+    func testProductWithoutPalmOil() {
+        let app = XCUIApplication()
+        app.buttons["ENTER"].tap()
+        
+        let productTextField = app.textFields["Enter product name"]
+        let product = "Waitrose Dark Chocolate Spread"
+        productTextField.tap()
+        productTextField.typeText(product)
+        app.buttons["SUBMIT"].tap()
+        
+        XCTAssertTrue(app.images["happy orangutan 1"].exists)
+        app.buttons["ENTER ANOTHER PRODUCT"].tap()
+    }
+    
+    func testProductNotFound() {
+        let app = XCUIApplication()
+        app.buttons["ENTER"].tap()
+        
+        let productTextField = app.textFields["Enter product name"]
+        let product = "Tesco Basics Cheese Slices"
+        productTextField.tap()
+        productTextField.typeText(product)
+        app.buttons["SUBMIT"].tap()
+        
+        XCTAssertTrue(app.images["confused orangutan"].exists)
         app.buttons["ENTER ANOTHER PRODUCT"].tap()
     }
 }
